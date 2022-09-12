05dbf9c305fb792758990aa1d51671d74987d0d9
shaya <shayaafkhami53@gmail.com>
2022-09-12 18:44:46 +0430

    version 1

---
+++ productInfo/info.js
@@ -0,0 +1,20 @@
+function getOneItemOfProducts() {
+    let html = ""
+    $.ajax({
+        url: "http://81.29.251.180:2345/products.json",
+        success: function (json) {
+            let products = json
+            for (let product of products) {
+                html +=   
+               
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            }
+        
